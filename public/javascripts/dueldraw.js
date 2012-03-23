5c134f6654188b3498818fbb3d3c185a63222719
Jeff Self <jeff.self@gmail.com>
2012-03-23 13:05:18 -0400

    Removed the clear canvas button

--- public/javascripts/dueldraw.js
+++ public/javascripts/dueldraw.js
@@ -1,5 +1,9 @@
 function clearCanvas() {
   var canvas = document.getElementById("canvas-1");
-  var context = canvas.getContext("2d");
-  context.clearRect(0, 0, canvas.width, canvas.height);
+  var ctx = canvas.getContext("2d");
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  canvas.width = canvas.width + 1;
+  width = canvas.width;
+  canvas.width = width - 1;
+  canvas.save();
 }
